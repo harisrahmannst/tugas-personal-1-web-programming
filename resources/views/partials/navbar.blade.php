003d3164e29ad520171efb379c34f2f44aed1d9b
Haris Nasution <haris.rahman@edi-indonesia.co.id>
2023-03-12 10:16:11 +0700

    first commit

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top bg-body-tertiary px-3 mb-3" id="navbar-main">
+    <div class="container-fluid">
+        <img src="/img/logo.png" alt="logo" width="60">
+        <a class="navbar-brand" href="#">DriveID</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav nav-pills ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#ceotalk">Who We?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#instrukture">Instructure</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#schedule">Schedule</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#pricing">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Our Cars</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<script src="js/bootstrap.bundle.min.js"></script>
+<!-- Navbar -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/smothscroll.js"></script>
---
+++ resources/views/partials/pricing.blade.php
@@ -0,0 +1,68 @@
+<section class="pricing" id="pricing">
+    <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+        <h1 class="display-4 fw-normal">Pricing</h1>
+        <p class="fs-5 text-muted"> We understand the need to ensure that there is a fair balance between the
+            cost of
+            obtaining your licence and the quality of the final skills transferred to the learner driver.</p>
+    </div>
+    </header>
+
+    <main>
+        <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal">Category A </h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$200<small class="text-muted fw-light">/mo</small>
+                        </h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>30 hours driving</li>
+                            <li>40 hours in class</li>
+                            <li>Practical Test</li>
+                            <li>Car or minivan</li>
+                            <li>-</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal">Category B</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$300<small class="text-muted fw-light">/mo</small>
+                        </h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>30 hours driving</li>
+                            <li>40 hours in class</li>
+                            <li>Practical Test</li>
+                            <li>Vehicle up to 3,500kg</li>
+                            <li>-</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm border-primary">
+                    <div class="card-header py-3 text-bg-primary border-primary">
+                        <h4 class="my-0 fw-normal">Enterprise</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$400<small class="text-muted fw-light">/mo</small>
+                        </h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>30 hours driving</li>
+                            <li>40 hours in class</li>
+                            <li>Practical Test</li>
+                            <li>Vehicle up to 3,500kg</li>
+                            <li>Trailer up to 750kg</li>
+                        </ul>
+                        <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
